378250d799ef48f5493000f12279427f8ddb4aee
swiftFox <rcwilson995@gmail.com>
2016-08-21 18:35:17 +0100

    Add front end form to submit message

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+var PORT = process.env.PORT || 3000;
 var express = require('express');
-const PORT = process.env.PORT || 3000;
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 app.use(express.static(__dirname + '/public'));
 
-io.on('connection', function ()  {
-    console.log('User connected via socket.io!');
+io.on('connection', function (socket) {
+	console.log('User connected via socket.io!');
+
+	socket.on('message', function (message) {
+		console.log('Message received: ' + message.text);
+
+		socket.broadcast.emit('message', message);
+	});
+
+	socket.emit('message', {
+		text: 'Welcome to the chat application!'
+	});
 });
 
 http.listen(PORT, function () {
-    console.log(`Server started on Port: ${PORT}`);
+	console.log('Server started!');
 });
